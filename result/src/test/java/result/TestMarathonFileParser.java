289be77ae280f1cc3d29ba937c5963f171a4acad
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-01-24 14:37:33 +0100

    working on file readers

--- result/src/test/java/result/TestMarathonFileParser.java
+++ result/src/test/java/result/TestMarathonFileParser.java
@@ -3,7 +3,7 @@ package result;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import result.marathon.MarathonFileParser;
+import result.marathon.MarathonFileReader;
 import result.marathon.MarathonResult;
 import java.util.List;
 
@@ -16,7 +16,7 @@ public class TestMarathonFileParser {
         // TODO: link to the correct place in repo, currently hard coded to my local file
         String startFile = "C:\\Users\\Tomas\\Desktop\\Acceptanstester\\Maraton\\acceptanstestM1\\starttider.txt";
         String endFile = "C:\\Users\\Tomas\\Desktop\\Acceptanstester\\Maraton\\acceptanstestM1\\maltider.txt";
-        list = MarathonFileParser.result(startFile, endFile);
+        list = MarathonFileReader.result(startFile, endFile);
     }
 
     @Test
