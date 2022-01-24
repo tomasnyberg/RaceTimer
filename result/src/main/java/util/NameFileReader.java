d60d5ff9a1814a1a88fbeb6d4264cc8a11ba58d5
joelpistora <joel2.pistora@gmail.com>
2022-01-24 14:25:26 +0100

    Added test files for NameFileReader

--- result/src/main/java/util/NameFileReader.java
+++ result/src/main/java/util/NameFileReader.java
@@ -4,13 +4,14 @@ import result.DriverEntry;
 import result.DriverList;
 
 public class NameFileReader extends AbstractFileReader{
+
     @Override
     public String file() {
-        return "C:\\Users\\jojog\\team05\\result\\src\\test\\java\\result\\namnfil.txt";
+        return "C:\\Users\\joel2\\IdeaProjects\\team05\\result\\src\\test\\java\\result\\namnfil.txt";
     }
 
     @Override
-    public Object CreateObject() {
+    public ArrayList<DriverEntry> CreateObject() {
         DriverList driverList = new DriverList();
         for(int i=2; i < data.size(); i+=2){
             DriverEntry driver = new DriverEntry(data.get(i), data.get(i+1));
