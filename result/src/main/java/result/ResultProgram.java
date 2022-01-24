77ab7df7b8e63add593756aa3cade241efabef09
JTh <julius.wikdahl_thunstrom.3373@student.lu.se>
2022-01-24 15:44:09 +0100

    Add M.1.

--- result/src/main/java/result/ResultProgram.java
+++ result/src/main/java/result/ResultProgram.java
@@ -1,26 +1,24 @@
 package result;
 
+import result.marathon.MarathonResult;
+import util.FileReader;
+
+import java.util.List;
+
+
 /**
  * The main program. TODO!
  */
 public class ResultProgram {
 
     public static void main(String[] args) {
-        // read arguments (args)
+        String startTimeFile = args[0];
+        String endTimeFile = args[1];
 
-        // Set up data structure
-
-        // Read input from files
-
-        // "computation"
-
-        // error checks?
-
-        // format result
-
-        // produce result file
+        List<MarathonResult> resultList = FileReader.result(startTimeFile, endTimeFile);
+        FileWriter.write(resultList);
 
         System.out.println("This is the result program!");
-
     }
+
 }
