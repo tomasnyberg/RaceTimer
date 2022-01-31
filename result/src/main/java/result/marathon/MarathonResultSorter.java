dc027050ed4a8628a2de17710cbbc27b1ca1756f
JTh <julius.wikdahl_thunstrom.3373@student.lu.se>
2022-01-31 09:59:39 +0100

    M7 Add MarathonResultSorter

---
+++ result/src/main/java/result/marathon/MarathonResultSorter.java
@@ -0,0 +1,19 @@
+package result.marathon;
+
+import result.marathon.MarathonResult;
+import result.marathon.MarathonResultRow;
+import util.ResultSorter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MarathonResultSorter extends ResultSorter<MarathonResult> {
+
+
+    @Override
+    public List<MarathonResult> sortResults(List<MarathonResult> results) {
+        return new ArrayList<>();
+    }
+
+
+}
