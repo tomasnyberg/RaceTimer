6fcef061e8d42ba7261eb32a5e950f48db3cdfb5
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-01-31 11:53:56 +0100

    M7 implemented sorting for both correct and incorrect times, and tests

--- result/src/main/java/result/marathon/MarathonResultSorter.java
+++ result/src/main/java/result/marathon/MarathonResultSorter.java
@@ -2,20 +2,42 @@ package result.marathon;
 
 import result.marathon.MarathonResult;
 import result.marathon.MarathonResultRow;
+import result.marathon.error.MarathonDecorator;
+import result.marathon.error.MissingEndTime;
+import result.marathon.error.MissingStartTime;
 import util.ResultSorter;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class MarathonResultSorter extends ResultSorter<MarathonResult> {
 
-
     @Override
     public List<MarathonResult> sortResults(List<MarathonResult> results) {
         List<MarathonResult> sorted = new ArrayList<>(results);
-        sorted.sort((mr1, mr2) -> mr1.getTotal().compareTo(mr2.getTotal()));
+        // If a result has a decorator, it should go last. Otherwise just compare total
+        // times
+        sorted.sort(new comp());
         return sorted;
     }
 
+    private class comp implements Comparator<MarathonResult> {
 
+        @Override
+        public int compare(MarathonResult mr1, MarathonResult mr2) {
+            if (!missingTime(mr1) && !missingTime(mr2)) {
+                return mr1.getTotal().compareTo(mr2.getTotal());
+            }
+            if(missingTime(mr1) && missingTime(mr2)) {
+                return mr1.getStart().compareTo(mr2.getStart());
+            }
+            return missingTime(mr1) ? 1:-1;
+        }
+
+        public boolean missingTime(MarathonResult mr) {
+            return mr instanceof MissingEndTime || mr instanceof MissingStartTime;
+        }
+
+    }
 }
--- result/src/test/java/result/TestMarathonResultSorter.java
+++ result/src/test/java/result/TestMarathonResultSorter.java
@@ -15,23 +15,30 @@ import java.util.*;
 public class TestMarathonResultSorter {
     List<MarathonResult> results;
     List<MarathonResult> sortedResults;
+    List<MarathonResult> incorrectResults;
+    List<MarathonResult> sortedIncorrectResults;
 
     @BeforeEach
-    public void setUp(){
-        // TODO: change these to correct directory
-        results = MarathonFileReader.result("../starttider.txt", "../maltider.txt");
+    public void setUp() {
+        String startFile = "../AcceptansTester/Maraton/acceptanstestM1/starttider.txt";
+        String endFile = "../AcceptansTester/Maraton/acceptanstestM1/maltider.txt";
+        String startFileIncorrect = "../AcceptansTester/Maraton/acceptanstestM4/starttider.txt";
+        String endFileIncorrect = "../AcceptansTester/Maraton/acceptanstestM4/maltider.txt";
+        results = MarathonFileReader.result(startFile, endFile);
         sortedResults = new MarathonResultSorter().sortResults(results);
+        incorrectResults = MarathonFileReader.result(startFileIncorrect, endFileIncorrect);
+        sortedIncorrectResults = new MarathonResultSorter().sortResults(incorrectResults);
     }
 
     @Test
-    public void testEmptyList(){
+    public void testEmptyList() {
         results = new ArrayList<>();
         List<MarathonResult> newResults = new MarathonResultSorter().sortResults(results);
         assertTrue(results.equals(newResults), "Not an empty result");
     }
 
     @Test
-    public void testFirstTimesAreRight(){
+    public void testFirstTimesAreRight() {
         String first = sortedResults.get(0).getTotal();
         String second = sortedResults.get(1).getTotal();
         String third = sortedResults.get(2).getTotal();
@@ -39,8 +46,21 @@ public class TestMarathonResultSorter {
         assertEquals(first, "01:03:06");
         // Driver 4 finishes in 12:03:00 -> 13:12:07 = 01:08:07
         assertEquals(second, "01:09:07");
-        // Driver 5 finishes in 01 12 07 
+        // Driver 5 finishes in 01 12 07
         assertEquals(third, "01:12:07");
+    }
+
+    @Test
+    public void testIncorrectFileSorting(){
+        assertEquals(5, incorrectResults.size());
+        String first = sortedIncorrectResults.get(0).getTotal();
+        String second = sortedIncorrectResults.get(1).getTotal();
+        String third = sortedIncorrectResults.get(2).getTotal();
+        String fourth = sortedIncorrectResults.get(3).getTotal();
+        String fifth = sortedIncorrectResults.get(4).getTotal();
+        assertEquals("00:12:07", first);
+        assertEquals("--:--:--", fourth);
+        assertEquals("--:--:--", fifth);
     }
 
 }
