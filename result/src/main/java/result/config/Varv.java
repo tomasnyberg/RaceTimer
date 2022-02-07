557fd06a5732b5ee0c758cbf27885afdfa1aac03
ChristopheEkendahl <Cekendahl00@gmail.com>
2022-02-07 15:16:14 +0100

    V3 Config fixes for mass start

--- result/src/main/java/result/config/Varv.java
+++ result/src/main/java/result/config/Varv.java
@@ -1,31 +1,50 @@
 package result.config;
 
 public class Varv {
-    private int minimumLaps;
-    private String minimumTime;
+  private int minimumLaps;
+  private String minimumTime;
+  private boolean massStart;
+  private String timeForMassStart;
 
-    public Varv() {
-      super();
-    }
-
-    public Varv(int minimumLaps, String minimumTime) {
-      this.minimumLaps = minimumLaps;
-      this.minimumTime = minimumTime;
-    }
-
-    public int getMinimumLaps() {
-      return minimumLaps;
-    }
-
-    public void setMinimumLaps(int minimumLaps) {
-      this.minimumLaps = minimumLaps;
-    }
-
-    public String getMinimumTime() {
-      return minimumTime;
-    }
-
-    public void setMinimumTime(String minimumTime) {
-      this.minimumTime = minimumTime;
-    }
+  public Varv() {
+    super();
   }
+
+  public Varv(int minimumLaps, String minimumTime) {
+    this.minimumLaps = minimumLaps;
+    this.minimumTime = minimumTime;
+  }
+
+  public int getMinimumLaps() {
+    return minimumLaps;
+  }
+
+  public void setMinimumLaps(int minimumLaps) {
+    this.minimumLaps = minimumLaps;
+  }
+
+  public String getMinimumTime() {
+    return minimumTime;
+  }
+
+  public void setMinimumTime(String minimumTime) {
+    this.minimumTime = minimumTime;
+  }
+
+  public boolean getMassStart() {
+    return massStart;
+  }
+
+  public void setMassStart(boolean massStart) {
+    this.massStart = massStart;
+  }
+
+  public String getTimeForMassStart() {
+    return timeForMassStart;
+  }
+
+  public void setTimeForMassStart(String timeForMassStart) {
+    this.timeForMassStart = timeForMassStart;
+  }
+
+}
