3eb2da34c0435c1386dd584cb312866fa913b5a8
Joel Pistora <joel2.pistora@gmail.com>
2022-02-07 14:30:41 +0100

    V1 adds readStartTime and readEndTimes

--- result/src/test/java/result/Varvlopp/TestVarvloppResult.java
+++ result/src/test/java/result/Varvlopp/TestVarvloppResult.java
@@ -11,16 +11,33 @@ public class TestVarvloppResult {
     @BeforeEach
     public void setup() {
         vlr = new VarvloppResult();
-        //TODO : fetch this from config class instead of hardcoding
     }
     
     @Test
     public void testReadStartTimes(){
         vlr.readStartTimes("../Acceptanstester/V/1/input/starttider.txt");
         assertEquals(5, vlr.drivers.size());
+        // for(VarvloppDriver vd: vlr.drivers){
+        //     System.out.println(vd.toString());
+        // }
+    }
+
+    @Test
+    public void testReadEndTimes() {
+        vlr.readEndTimes("../Acceptanstester/V/1/input/maltider.txt");
+        assertEquals(5, vlr.drivers.size());
+        // for(VarvloppDriver vd: vlr.drivers){
+        //     System.out.println(vd.toString());
+        // }
+    }
+
+    @Test
+    public void testReadStartAndEndTimes() {
+        vlr.readStartTimes("../Acceptanstester/V/1/input/starttider.txt");
+        vlr.readEndTimes("../Acceptanstester/V/1/input/maltider.txt");
+        assertEquals(5, vlr.drivers.size());
         for(VarvloppDriver vd: vlr.drivers){
             System.out.println(vd.toString());
         }
     }
-
 }
