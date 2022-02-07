42fd9cb3a7410564b2009e394736d88fa7837e1f
Lucas Boberg <lucaspboberg@gmail.com>
2022-02-07 09:34:28 +0100

    Implements custom input and output files

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 ### To run / build the register subproject : ./gradlew :register:run
 
-### To run / build result project you have to specify minimum time that will be used to find invalid total times: ./gradlew :result:run --args='hh:mm:ss'
+### To run / build result project you have to specify arguments: ./gradlew :result:run --args='hh:mm:ss nameFilePath startTimesPath endTimesPath resultPath'
 
 ### Currently no docs file in the repo like before, also some slight changes in the gradle files / structure from the other zero feature release
--- result/src/test/java/result/TestMarathonFileReader.java
+++ result/src/test/java/result/TestMarathonFileReader.java
@@ -7,34 +7,34 @@ import result.marathon.MarathonFileReader;
 import result.marathon.MarathonResult;
 import java.util.List;
 
-
 public class TestMarathonFileReader {
-    private List<MarathonResult> list;
-    private String minimumTime = "00:00:00";
+  private List<MarathonResult> list;
+  private String minimumTime = "00:00:00";
 
-    @BeforeEach
-    public void setUp(){
-        String startFile = "input/starttider.txt";
-        String endFile = "input/maltider.txt";
-        list = MarathonFileReader.result(startFile, endFile, minimumTime);
-    }
+  @BeforeEach
+  public void setUp() {
+    String nameFile = "input/namnfil.txt";
+    String startFile = "input/starttider.txt";
+    String endFile = "input/maltider.txt";
+    list = MarathonFileReader.result(nameFile, startFile, endFile, minimumTime);
+  }
 
-    @Test
-    public void testReadsAll(){
-        assertEquals(5 , list.size(), "incorrect amount of items");
-    }
+  @Test
+  public void testReadsAll() {
+    assertEquals(5, list.size(), "incorrect amount of items");
+  }
 
-    @Test
-    public void testFirstStartTimes(){
-        assertEquals("12:00:00", list.get(0).getStart());
-        assertEquals("12:01:00", list.get(1).getStart());
-        assertEquals("12:02:00", list.get(2).getStart());
-    }
+  @Test
+  public void testFirstStartTimes() {
+    assertEquals("12:00:00", list.get(0).getStart());
+    assertEquals("12:01:00", list.get(1).getStart());
+    assertEquals("12:02:00", list.get(2).getStart());
+  }
 
-    @Test
-    public void testFirstEndTimes(){
-        assertEquals("13:23:34", list.get(0).getEnd());
-        assertEquals("13:15:16", list.get(1).getEnd());
-        assertEquals("13:05:06", list.get(2).getEnd());
-    }
+  @Test
+  public void testFirstEndTimes() {
+    assertEquals("13:23:34", list.get(0).getEnd());
+    assertEquals("13:15:16", list.get(1).getEnd());
+    assertEquals("13:05:06", list.get(2).getEnd());
+  }
 }
