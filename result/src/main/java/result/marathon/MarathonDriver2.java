24b8918f062f78fa26364196d798898dccf816b6
Daniel Amir <danielamir1998@gmail.com>
2022-02-14 08:53:38 +0100

    added new marathon driver and result classes that extends abstract (temp 02)

---
+++ result/src/main/java/result/marathon/MarathonDriver2.java
@@ -0,0 +1,10 @@
+package result.marathon;
+
+import result.AbstractDriver;
+
+public class MarathonDriver2 extends AbstractDriver {
+    @Override
+    protected String getErrors() {
+        return null;
+    }
+}
---
+++ result/src/main/java/result/marathon/MarathonResult2.java
@@ -0,0 +1,20 @@
+package result.marathon;
+
+import result.AbstractResult;
+
+public class MarathonResult2 extends AbstractResult {
+    @Override
+    public void generateResult() {
+
+    }
+
+    @Override
+    protected void readTimes(String filePath, boolean start) {
+
+    }
+
+    @Override
+    protected void readNames(String nameFile) {
+
+    }
+}
