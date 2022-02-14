6b64370dd84b3822c4326ebe74d71f6223c4b01f
Joel Pistora <joel2.pistora@gmail.com>
2022-02-14 10:58:37 +0100

    Refactor: AbstractResult fix

--- result/src/main/java/result/AbstractResult.java
+++ result/src/main/java/result/AbstractResult.java
@@ -20,21 +20,56 @@ public abstract class AbstractResult {
     public abstract void generateResult();
 
     // both read start times and end times read the times, so we have a method for the common things
-    // returns something that the other methods can use to fill in the info for the drivers
-    protected abstract void readTimes(String filePath, boolean start);
+  // returns something that the other methods can use to fill in the info for the drivers
+  private void readTimes(String filePath, boolean start) {
+    List<String> lines = readFile(filePath);
+    for (String line : lines) {
+      String[] split = line.split("; ");
+      String time = split[1];
+      String driverNumber = split[0];
+      boolean found = false;
+      for (int i = 0; i < drivers.size(); i++) {
+        if (drivers.get(i).getDriverNumber().equals(driverNumber)) {
+          if (start) {
+            drivers.get(i).addStartTime(time);
+          } else {
+            drivers.get(i).addGoalTime(time);
+          }
+          found = true;
+          break;
+        }
+      }
+      if (!found) {
+        AbstractDriver driver = newDriver(driverNumber, config);
+        if (start) {
+          driver.addStartTime(time);
+        } else {
+          driver.addGoalTime(time);
+        }
+        drivers.add(driver);
+      }
+    }
+  }
 
-    // Reads starttimes from a starttimefile, and sets the start times for the respective driver number
-    // If we have not seen this drivernumber so far, we create a new driver
-    protected abstract void readStartTimes();
+  // Reads starttimes from a starttimefile, and sets the start times for the respective driver
+  // number
+  // If we have not seen this drivernumber so far, we create a new driver
+  protected void readStartTimes() {
+    readTimes(config.getLap().getStartTimesFile(), true);
+  }
 
-    // Reads endtimes from a file, and adds the end times for the respective driver number
-    // If we have not seen this drivernumber so far, we create a new driver
-    protected abstract void readEndTimes();
+  // Reads endtimes from a file, and adds the end times for the respective driver number
+  // If we have not seen this drivernumber so far, we create a new driver
+  protected void readEndTimes() {
+    for (String endFile : config.getLap().getGoalTimesFiles()) {
+      readTimes(endFile, false);
+    }
+  }
 
 
     // Reads names from a file
     // If we have not seen this drivernumber so far, we create a new driver
-    protected abstract void readNames() {
+    protected void readNames() {
         List<String> lines = readFile(config.getNameFile());
         for (int i = 1; i < lines.size(); i++) {
             String[] split = lines.get(i).split("; ");
