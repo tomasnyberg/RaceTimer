f0f2aa3cc2179be853ffbd585204c7b6f4451c51
JTh <julius.wikdahl_thunstrom.3373@student.lu.se>
2022-02-28 11:08:10 +0100

    Add comment "explaining" fix for encodings

--- result/src/main/java/util/OSString.java
+++ result/src/main/java/util/OSString.java
@@ -5,6 +5,9 @@ import java.nio.charset.StandardCharsets;
 public class OSString {
 
     public static String convert(String s) {
+        /*
+        * Hacky solution to fix character encodings on Windows.
+        * */
         if (System.getProperty("os.name").toLowerCase().startsWith("win")) {
             return new String(s.getBytes(StandardCharsets.ISO_8859_1), StandardCharsets.UTF_8);
         }
