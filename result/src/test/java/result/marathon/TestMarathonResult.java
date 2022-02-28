b0b8f04b0794e5f5a8c759668d1bff89cde1b922
on5631 <on5631ne-s@student.lu.se>
2022-02-28 13:40:01 +0100

    Fixed tests strings

--- result/src/test/java/result/marathon/TestMarathonResult.java
+++ result/src/test/java/result/marathon/TestMarathonResult.java
@@ -6,6 +6,7 @@ import result.AbstractDriver;
 import result.config.Config;
 import result.config.Marathon;
 import result.marathon.MarathonResult;
+import util.OSString;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -60,7 +61,7 @@ public class TestMarathonResult {
                              + "2; Bengt Bsson; --:--:--; Start?; 13:15:16\n"
                              + "3; Chris Csson; 01:03:06; 12:02:00; 13:05:06; Flera måltider? 13:07:08\n"
                              + "4; David Dsson; 01:09:07; 12:03:00; 13:12:07; Flera starttider? 12:12:00\n"
-                             + "5; Erik Esson; 00:12:07; 12:04:00; 12:16:07; Omöjlig Totaltid?\n";
+                             + OSString.convert("5; Erik Esson; 00:12:07; 12:04:00; 12:16:07; Omöjlig Totaltid?\n");
     String mrResultString = "";
     for (AbstractDriver driver : mr.drivers) {
       mrResultString += driver.toString() + "\n";
